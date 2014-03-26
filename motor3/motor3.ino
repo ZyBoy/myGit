1a2e343ba7130b4cf8f2a459d328373ae8366374
ZyBoy <zyboy.ru@gmail.com>
2014-03-26 23:05:23 +0400

    Небольшие исправления и отладка

--- motor3/motor3.ino
+++ motor3/motor3.ino
@@ -13,7 +13,7 @@ Servo servo1;
 int Trig = 18; // аналоговые выходы могут работать как цифровые. см. http://zelectro.com.ua/Adafruit_motor_shield
 int Echo = 19; 
 unsigned int time_us=0; // Переменная для хранения временного интервала
-unsigned int distance_sm=0; // Переменная для хранения расстояния в сантиметрах
+unsigned int distance_sm=0; // Переменная для хранения расстояния в сантиметрах (по умолчанию 0, что бы оглядеться)
 unsigned int vDistance_sm=0;
 
 int i;
@@ -25,7 +25,7 @@ void setup() {
   pinMode(Trig, OUTPUT);
   pinMode(Echo, INPUT);
   // Задаем максимальную скорость вращения моторов (аналог работы PWM) 
-  motor1.setSpeed(255);
+ /* motor1.setSpeed(255);
   motor1.run(RELEASE);
   motor2.setSpeed(255);
   motor2.run(RELEASE);
@@ -33,7 +33,7 @@ void setup() {
   motor3.run(RELEASE);
   motor4.setSpeed(255);
   motor4.run(RELEASE);
-  
+  */
   servo1.attach(9);  // Подключим объект "servo1" на "девятый" пин.
   servo1.write(90);  // Начальная позиция - посередке )))
 }
@@ -72,37 +72,37 @@ void stop() {
 void turn2left() {
     motor1.run(FORWARD);
     motor2.run(BACKWARD);
-    for (i=0; i<25; i++) {
+    for (i=0; i<10; i++) {
       motor1.setSpeed(i*10+5); 
       motor2.setSpeed(i*10+5); 
       delay(5);
     }
     motor3.run(BACKWARD);
     motor4.run(FORWARD);
-    for (i=0; i<25; i++) {
+    for (i=0; i<10; i++) {
       motor3.setSpeed(i*10+5); 
       motor4.setSpeed(i*10+5); 
       delay(5);
 	}
-    delay(500);
+  //  delay(100);
 }
 
 void turn2right() {
     motor1.run(BACKWARD);
     motor2.run(FORWARD);
-    for (i=0; i<25; i++) {
+    for (i=0; i<10; i++) {
       motor1.setSpeed(i*10+5); 
       motor2.setSpeed(i*10+5); 
       delay(5);
     }
     motor3.run(FORWARD);
     motor4.run(BACKWARD);
-    for (i=0; i<25; i++) {
+    for (i=0; i<10; i++) {
       motor3.setSpeed(i*10+5); 
       motor4.setSpeed(i*10+5); 
       delay(5);
 	}
-    delay(500);
+  //  delay(100);
 }
 
 int sonar() // функция типа int, потому что возвращается значенеи типа int )))
@@ -118,38 +118,37 @@ int sonar() // функция типа int, потому что возвраща
 
 void loop() 
 {
-
-  distance_sm = sonar();
-  delay(500); // пауза легкая, что бы не частить.
-  
-  if (distance_sm > 20) // едем пока есть куда
-  {
-    gogogo(FORWARD, 25); // вперед
-    // строки ниже закомментил, т.к. едем не по времени, а пока не упремся во что-нибудь...
-    //delay(5000); // ехать 5 секунд 
-    //stop(); // стоять, бояться!
-  }
-  
-  if (distance_sm <= 20) // разворачиваемся, пока не появится куда ехать
+ if (distance_sm <= 30) // разворачиваемся, пока не появится куда ехать
   {
     stop(); // стоять, бояться!
 	
-	servo1.write(10);  // Посмотрим налево
+	servo1.write(50);  // Посмотрим налево
+        delay(500); // пауза легкая
 	leftside = sonar();
 	
-	servo1.write(170);  // Посмотрим направо
-	rightside = sonar();
+	servo1.write(120);  // Посмотрим направо
+        delay(500); // пауза легкая
+        rightside = sonar();
 	
 	servo1.write(90);  // Посмотрим направо
 	
-	if (leftside > rightside)
-	  {
-	    turn2left();
-	  }
-	else
+       if (leftside > rightside)
 	  {
 	    turn2right();
 	  }
-	
+	else
+	  {
+	    turn2left();
+	  }
+  }
+  distance_sm = sonar();
+  // delay(500); // пауза легкая, что бы не частить.
+  
+  if (distance_sm > 30) // едем пока есть куда
+  {
+    gogogo(BACKWARD, 5); // вперед
+    // строки ниже закомментил, т.к. едем не по времени, а пока не упремся во что-нибудь...
+    //delay(5000); // ехать 5 секунд 
+    //stop(); // стоять, бояться!
   }
 }
