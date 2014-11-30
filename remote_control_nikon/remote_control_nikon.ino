7edf5347405d76c6cc9863497ca1b36e01c78171
ZyBoy <zyboy.ru@gmail.com>
2014-12-01 00:51:05 +0400

    Пульт с экраном будет + кнопки для настройки (время, количество и т.п., нужно продумать)

--- remote_control_nikon/remote_control_nikon.ino
+++ remote_control_nikon/remote_control_nikon.ino
@@ -1,13 +1,28 @@
+#include "U8glib.h"
+#include "utility\my5x7rus.h"
 
-int outPin = 12;
+int outPin = 12; // IR-светодиод
+int inPinOk = 3;  // кнопка [Ok]
+int inPinUp = 4;  // кнопка [▲]
+int inPinDw = 5;  // кнопка [▼]
+int inPinLf = 6;  // кнопка [◄]
+int inPinRt = 7;  // кнопка [►]
+
+// инициализация дисплея
+U8GLIB_PCF8812 u8g(13, 11, 10, 9, 8);		// SPI Com: SCK = 13, MOSI = 11, CS = 10, A0 = 9, Reset = 8
+
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(outPin, OUTPUT);
-  digitalWrite(outPin, LOW);
+  u8g.setRot180(); // Перевернем экран на 180 градусов
+
+  pinMode(outPin, OUTPUT);   // зададим режим порта OUTPUT
+  digitalWrite(outPin, LOW); // диод по умолчанию погашен
+
+  pinMode(inPinOk, INPUT);    // зададим режим порта INPUT
 }
 
-void shot(void){
+// Функция которая выдает кодированную команду для фотоаппарата
+void shot(void) {
   digitalWrite(outPin, HIGH);   // sets the pin on
   delayMicroseconds(2000);        // pauses
   digitalWrite(outPin, LOW);    // sets the pin off
@@ -25,7 +40,7 @@ void shot(void){
 
   digitalWrite(outPin, HIGH);   // sets the pin on
   delayMicroseconds(400);        // pauses
-  
+
   digitalWrite(outPin, LOW);
 }
 
@@ -34,4 +49,12 @@ void loop() {
   // put your main code here, to run repeatedly:
   shot();
   delay(2000);        // pauses
+
+
+
+
+
+
+
+
 }
