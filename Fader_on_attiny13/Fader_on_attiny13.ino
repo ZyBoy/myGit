ac76614b8e9cb44aa94165e15ff2e9bccb3bd2e1
ZyBoy <zyboy.ru@gmail.com>
2015-11-20 21:26:42 +0400

    Рандом незаменим

--- Fader_on_attiny13/Fader_on_attiny13.ino
+++ Fader_on_attiny13/Fader_on_attiny13.ino
@@ -1,3 +1,6 @@
+byte i1 = 0;
+byte i2 = 0;
+
 void setup() {
    for (byte i=0;i<5;i++) {
      pinMode(i, OUTPUT);
@@ -5,15 +8,18 @@ void setup() {
 }
 
 void loop() {
-  //byte i = random(0, 5);
+  byte i1 = random(0, 5);
 
   // вдруг повезет
-  byte i = millis()%6; // вместо рандома получаем время с момента запуска и берем остаток от деления на 6 (значения от 0 до 5)
+  //i1 = millis()%6; // вместо рандома получаем время с момента запуска и берем остаток от деления на 6 (значения от 0 до 5)
 
-  digitalWrite(i,HIGH);
-    delay(15);
-  digitalWrite(i,LOW);
-    delay(1);
+  digitalWrite(i1,HIGH);
+    delay(7);
+  digitalWrite(i2,LOW);
+
+  i2 = i1;
+
+
 }
 
 
